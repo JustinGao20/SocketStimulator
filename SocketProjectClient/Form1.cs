675fc5ef4abf97c6941239031b84f73c105847c0
JustinGao20 <junjieg2@illinois.edu>
2021-07-21 17:25:38 +0800

    final version

--- SocketProjectClient/Form1.cs
+++ SocketProjectClient/Form1.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
 
 
 namespace SocketProjectClient
@@ -23,6 +25,7 @@ namespace SocketProjectClient
         //创建sockets对象
         Socket socketCom;                   //创建用于通信的socket对象
         Socket socketListener;              //创建用于监听的socket对象
+        Thread receiveSocket;               //创建接受服务端发送消息的线程
 
         //定义回调
         private delegate void setTextValueCallBack(string callback); //定义显示消息提醒的回调
@@ -35,19 +38,70 @@ namespace SocketProjectClient
         //当用户点击连接的按钮时
         private void button1_Click(object sender, EventArgs e)
         {
-            //当用户点击开始连接的的时候，在客户端实例化一个socket对象来发送ip地址和端口号,基于tcp协议
-            socketCom = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                //当用户点击开始连接的的时候，在客户端实例化一个socket对象来发送ip地址和端口号,基于tcp协议
+                socketCom = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-            //输入ip地址
-            IPAddress IP = IPAddress.Parse(this.textBox1.Text.Trim());
+                //输入ip地址
+                IPAddress IP = IPAddress.Parse(this.textBox1.Text.Trim());
 
-            //绑定端口号准备把信息发送出去
-            socketCom.Connect(IP, Convert.ToInt32(this.textBox2.Text.Trim()));
+                //绑定端口号准备把信息发送出去
+                socketCom.Connect(IP, Convert.ToInt32(this.textBox2.Text.Trim()));
 
-            //将回调实例化
-            setTextValue = new setTextValueCallBack(setValue);
-            receiveMessageValue = new receiveMessageCallBack(setValue);
-            this.richTextBox1.Invoke(setTextValue,"系统连接成功!");
+                //将回调实例化
+                setTextValue = new setTextValueCallBack(setValue);
+                receiveMessageValue = new receiveMessageCallBack(setValue);
+                this.richTextBox1.Invoke(setTextValue, "系统连接成功!");
+
+                //创建一个新的线程不停的接受服务端发送消息的线程
+                receiveSocket = new Thread(new ThreadStart(Receive));
+
+                //后台线程，随着主线程停止而停止
+                receiveSocket.IsBackground = true;
+
+                //改变系统的线程状态
+                receiveSocket.Start();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("连接服务端的时候发生错误" + err.Message);
+            }
+        }
+
+        //接受服务器发送的消息
+        private void Receive()
+        {
+            try
+            {
+                while (true)
+                {
+                    //服务端连接后，定义服务器能接受客户发送消息的最大字节数量
+                    byte[] max = new byte[4096];
+
+                    //实际接收到的有效字节数量
+                    int status = socketCom.Receive(max);
+
+                    if (status == 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        if(max[0] == 0)
+                        {
+                            string message = Encoding.Default.GetString(max, 1, status - 1);
+                            //使用回调，发送消息
+                            string display = "接收" + socketCom.RemoteEndPoint + "发送的消息：" + message;
+                            richTextBox1.Invoke(receiveMessageValue, display);
+                        }
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("接收服务端消息的时候发生错误" + err.Message);
+            }
         }
 
         //当按下发送消息按钮的时候，客户端给服务端发送消息
@@ -67,21 +121,25 @@ namespace SocketProjectClient
             }
             catch (Exception err)
             {
-                MessageBox.Show("发送消息的时候出现错误："+ err.Message);
+                MessageBox.Show("发生消息出错：" + err.Message);
             }
         }
+
+        //
+
 
         //当按下终止连接的按钮的时候，关闭通讯函数和线程
         private void button3_Click(object sender, EventArgs e)
         {
             socketCom.Close();
+            receiveSocket.Abort();
         }
 
 
         //准备在消息栏把消息打印出来
         private void setValue(string textValue)
         {
-            this.richTextBox1.AppendText(textValue + "/n");
+            this.richTextBox1.AppendText(textValue + "\n");
         }
     }
 }
